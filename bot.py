fe55a6b706e3b2b4b70332888a4c6f97ce1d72b9
Eugen <cmrrus@rambler.ru>
2021-03-03 17:58:54 +0300

    Загрузил картинки

--- bot.py
+++ bot.py
@@ -9,6 +9,9 @@
 import ephem
 import logging
 import setting
+from emoji import emojize
+from glob import glob
+from random import choice, randint
 from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
 
 logging.basicConfig(filename = 'bot.log',level = logging.INFO)
@@ -40,12 +43,51 @@ def planet_user (update,context):
 def greet_user(update,context):
     print('Вызван /start') # написать в консоле
     #print(update) # информация о пользователе 
-    update.message.reply_text('Здравствуй,пользователь!') #написать пользователю
+    context.user_data['emoji'] = get_smile(context.user_data)#обьявление того что мы хотим использовать нужые смайлики
+    update.message.reply_text(f"Здравствуй,пользователь! {context.user_data['emoji']}") #написать пользователю
 
 def talk_to_me(update,context):
+    context.user_data['emoji'] = get_smile(context.user_data)
     text = update.message.text
     print(text)
-    update.message.reply_text(text)
+    update.message.reply_text(f"{text},{context.user_data['emoji']}")
+
+def get_smile(user_data):
+    if 'emoji' not in user_data:
+        smile = choice(setting.USER_EMOJI)
+        return emojize(smile,use_aliases=True)
+    return user_data['emoji'] # аналогия с else
+
+
+def play_random_numbers (user_number):
+    bot_number = randint(user_number - 10, user_number + 10)
+    if user_number > bot_number:
+        message = f'Ваше число  {user_number}, мое число {bot_number}, вы выиграли'
+    elif user_number == bot_number:
+         message = f'Ваше число  {user_number}, мое число {bot_number}, ничья'
+    else:
+         message = f'Ваше число  {user_number}, мое число {bot_number}, проиграл'
+    return message
+
+
+def guess_number(update,context):
+    print(context.args)
+    if context.args:
+        try:
+            user_number = int(context.args[0]) # от нуля 
+            message = play_random_numbers(user_number)
+        except (TypeError,ValueError):
+            message = 'Введите целое число'
+
+    else:
+        message = 'Введи число'
+    update.message.reply_text(message)
+def send_cat_picture(update,context):
+    cat_photo_list = glob('images/cat*.jp*g')
+    cat_photo_filename = choice(cat_photo_list)
+    chat_id = update.effective_chat.id #ид чата
+    context.bot.send_photo(chat_id= chat_id,photo=open(cat_photo_filename,'rb'))
+#открываем контекст бот отправить фото в чат ид фото открыть (рандомное фот ов бинарном формате)
 def main():
     # Создаем бота и передаем ему ключ для авторизации на серверах Telegram
     mybot = Updater(setting.API_KEY, use_context=True)
@@ -53,6 +95,8 @@ def main():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start',greet_user))
     dp.add_handler(CommandHandler('planet',planet_user))
+    dp.add_handler(CommandHandler('quess',guess_number))
+    dp.add_handler(CommandHandler('cat', send_cat_picture))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
    
     logging.info('Бот стартовал')
